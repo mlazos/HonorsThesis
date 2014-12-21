d437e9fd99cbd4bcec67d3eedf667dcbfa9abe02
mlazos <Michael_Lazos@brown.edu>
2014-12-20 21:25:01 -0500

    labelled custom dataset, modified system to interpret it, KILLING IT

--- SVMFullFeatureSet/runVisualization.m
+++ SVMFullFeatureSet/runVisualization.m
@@ -1,16 +1,14 @@
-%tileSize = 40;
-%trainSVM;
-for i = test_inds(10:15)'
-    imseg = imsegs(i);
+for i = test_inds(10:20)
+    imseg = image_data(i);
     [im, labels, segim, truth, numCorrect, total] = classifySingleImage(imseg, SVMStruct, classifiers.vert_classifier,classifiers.horz_classifier,segment_density, tileSize, ones(1,82));
     learned = visualizeClassification(im,labels,tileSize);
     correct = visualizeClassification(im,truth, tileSize);
     figure;
     subplot(1,2,1);
     imagesc(learned);
-    title([imseg.imname, ' learned']);
+    title([imseg.name, ' learned']);
     subplot(1,2,2);
     imagesc(correct);
-    title([imseg.imname, ' truth']);
+    title([imseg.name, ' truth']);
 end
 
--- SVMFullFeatureSet/trainSVM.m
+++ SVMFullFeatureSet/trainSVM.m
@@ -1,69 +1,18 @@
-% 'training...'
-% C = [2.9,2.95,3,3.05,3.1];
-% numSVMs = length(C);
-% FEATURES = features(1:numFeatureVecs,:);
-% LABELS = truthLabels(1:numFeatureVecs);
-% BETA = ones(82,1);
-% parfor i = 1:numSVMs
-%     LSVMrbf(i) = svmtrain(LABELS, FEATURES,sprintf('-s 0 -c %d -t 2 -g 1/82',C(i)));
-% end
-% 'training... done!'
-
-'evaluating...'
-evaluateSVM;
-'evaluating... done!'
-
-save EVALUATINGCS
-
-linSVM = fitcsvm(features(1:numFeatureVecs,:), truthLabels(1:numFeatureVecs),'Standardize',true);
-
 'training...'
-C = [2.9,2.95,3,3.05,3.1];
+C = [3];%2.9,2.95,3,3.05,3.1];
 numSVMs = length(C);
-BETA = linSVM.Beta;
-FEATURES = features(1:numFeatureVecs,abs(linSVM.Beta) > .1);
+FEATURES = features(1:numFeatureVecs,:);
 LABELS = truthLabels(1:numFeatureVecs);
-
+BETA = ones(82,1);
 parfor i = 1:numSVMs
     LSVMrbf(i) = svmtrain(LABELS, FEATURES,sprintf('-s 0 -c %d -t 2 -g 1/82',C(i)));
 end
 'training... done!'
 
-'evaluating...'
-evaluateSVM;
-'evaluating... done!'
-
-save EVALUATINGREDUCEDFEATURESET
 
 
 
 
 
-% SVMStruct = fitcsvm(features(1:numFeatureVecs,:), truthLabels(1:numFeatureVecs),'KernelFunction','polynomial','Standardize',true);
-% '1...'
-% SVMStructRBF = fitcsvm(features(1:numFeatureVecs,:), truthLabels(1:numFeatureVecs),'KernelFunction','rbf','Standardize',true);
-% 
-% '5...'
-% libSVMStructrbf = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c .5 -t 2 -g 1/82 ');
-% '6...'
-% libSVMStructrbf1 = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c 1 -t 2 -g 1/82 ');
-% '7...' 
-% libSVMStructrbf2 = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c 5 -t 2 -g 1/82 ');
-% '8...'
-% libSVMStructrbf3 = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c 10 -t 2 -g 1/82 ');
-% '9...'
-% libSVMStructrbf4 = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c 7 -t 2 -g 1/82 ');
-% 
-% libSVMStructrbf6 = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c 5.5 -t 2 -g 1 ');
-% 
-% libSVMStructrbf7 = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c 5.3 -t 2 -g 1/82 ');
-% 
-% libSVMStructrbf8 = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c 4.8 -t 2 -g 1/82 ');
-% 
-% libSVMStrcutrbf9 = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c 20 -t 2 -g 1/82 ');
-% 
-% libSVMStructrbf10 = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c 4.5 -t 2 -g 1/82 ');
-% 
-% libSVMStructrbf11 = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c 4.3 -t 2 -g 1/82 ');
-% 
-% libSVMStrcutrbf12 = svmtrain(truthLabels(1:numFeatureVecs),features(1:numFeatureVecs,:),'-s 0 -c 50 -t 2 -g 1/82 ');
+
+
