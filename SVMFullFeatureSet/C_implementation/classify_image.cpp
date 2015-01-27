feda86970e9081eb0bb81dde66203fd472cf945f
Michael Lazos <mlazos@cs.brown.edu>
2015-01-27 18:24:53 -0500

    successfully calculated features for an image and stored them in a matrix

--- SVMFullFeatureSet/C_implementation/classify_image.cpp
+++ SVMFullFeatureSet/C_implementation/classify_image.cpp
@@ -10,6 +10,8 @@
 #include "filter.h"
 #include "canny.h"
 #include "edge_centers.h"
+#include "matrix.h"
+#include <math.h>
 
 using namespace vlib;
 using namespace hsv;
@@ -30,30 +32,35 @@ int main(int argc, char **argv) {
   image<uchar> *gray = imageRGBtoGRAY(input);
   image<float> *smoothed = smooth(gray, 1.0);
   image<uchar> *edgeim = canny(smoothed, 1.0);
-   
+  
   const int tile_size = 30;
   int width = hsv_im->width();
   int height = hsv_im->height();
   
   int num_tiles = (width/tile_size) * (height/tile_size);
-  float test[6];
 
-
+  matrix<float>* features = new matrix<float>(num_tiles, 16, true);
+  int num_features = 0;
   //calcualte features over all tiles
   for(int row = 0; row + tile_size < height; row += tile_size) {
     for(int col = 0; col + tile_size < width; col += tile_size) {
-      //hsv_means(hsv_im, col, col + tile_size, row, row + tile_size, test);
-      //hue_histogram_features(hsv_im, col, col + tile_size, row, row + tile_size, 0, 1, 5, test); 
-      //sat_histogram_features(hsv_im, col, col + tile_size, row, row + tile_size, 0, 1, 3, test); 
-      //location_features(row, row + tile_size, test);
-      edge_centers(edgeim, col, col + tile_size, row, row + tile_size, test);          
-      printf("%f, %f\n", test[0], test[1]); 
-      //printf("%f\n",*test/height);
-      //printf("%d, %f, %f, %f, %f, %f, %f \n",col, test[0],test[1],test[2], test[3], test[4], test[5]);
+      hsv_means(hsv_im, col, col + tile_size, row, row + tile_size, matPtr(features, num_features, 0));
+      hue_histogram_features(hsv_im, col, col + tile_size, row, row + tile_size, 0, 1, 5, matPtr(features, num_features, 3)); 
+      sat_histogram_features(hsv_im, col, col + tile_size, row, row + tile_size, 0, 1, 3, matPtr(features, num_features, 9)); 
+      location_features(row, row + tile_size, input->height(), matPtr(features, num_features, 13));
+      edge_centers(edgeim, col, col + tile_size, row, row + tile_size, matPtr(features,num_features, 14));          
+
+      for(int ind = 0; ind < 16; ind++) {
+        printf("%f ", matRef(features, num_features, ind));
+      } 
+      printf("\n");
+
+      num_features++;
     }
   }
 
 
+  
 
   delete input;
   delete smoothed;
