68e116a96c4c9cb7c6178ccd4ef3f2fa8b0caa7b
Michael Lazos <Mike@Michaels-MacBook-Pro-4.local>
2015-03-06 17:51:27 -0500

    determined optimal model is poly3 with C=3, finalized feature set

--- SVMFullFeatureSet/trainSVM.m
+++ SVMFullFeatureSet/trainSVM.m
@@ -1,8 +1,8 @@
 'training...'
 C = 3;%2.9,2.95,3,3.05,3.1];
-numSVMs = length(C);
-parfor i = 1:numSVMs
-    LSVMrbf(i) = svmtrain(LABELS, FEATURES(:,logical(BETA)),sprintf('-s 0 -c %d -t 2 -g 1/82',C(i)));
+
+parfor i = 1:size(BETA, 1)
+    LSVMrbf(i) = svmtrain(LABELS, FEATURES(:,logical(BETA(i,:))),sprintf('-s 0 -c %d -t 2 -g 1/82',C));
 end
 clear numSVMs C
 'training... done!'
